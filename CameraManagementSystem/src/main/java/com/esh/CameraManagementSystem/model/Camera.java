c861f1e79881bf418b4c0b427d05b52b9785155b
35569 <35569@DESKTOP-3RRV2NK>
2020-08-13 15:25:34 +0200

    Create model package with attributes.Closed #3

---
+++ CameraManagementSystem/src/main/java/com/esh/CameraManagementSystem/model/Camera.java
@@ -0,0 +1,83 @@
+package com.esh.CameraManagementSystem.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "camera")
+public class Camera {
+	
+	
+	@Id
+	private Long id;
+	private String name;
+	private String model;
+	private String resolution;
+	private String ip;
+	
+	//**Default Constructor
+	public Camera() {
+	}
+	
+	//**Constructor with parameter
+	public Camera(Long id, String name, String model, String resolution, String ip) {
+		this.id = id;
+		this.name = name;
+		this.model = model;
+		this.resolution = resolution;
+		this.ip = ip;
+	}
+
+	//**Getter and Setter
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getResolution() {
+		return resolution;
+	}
+
+	public void setResolution(String resolution) {
+		this.resolution = resolution;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	@Override
+	public String toString() {
+		return "Camera [id=" + id + ", name=" + name + ", model=" + model + ", resolution=" + resolution + ", ip=" + ip
+				+ "]";
+	}
+	
+	
+	
+	
+	
+	
+
+}
