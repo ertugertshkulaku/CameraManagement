ed2812009823356049651973584c2fc8e8e213ce
35569 <35569@DESKTOP-3RRV2NK>
2020-08-13 15:06:50 +0200

    Initialize Project.Closed #1

---
+++ CameraManagementSystem/src/main/java/com/esh/CameraManagementSystem/CameraManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.esh.CameraManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameraManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameraManagementSystemApplication.class, args);
+	}
+
+}
---
+++ CameraManagementSystem/src/test/java/com/esh/CameraManagementSystem/CameraManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esh.CameraManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CameraManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
