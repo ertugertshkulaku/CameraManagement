34bcce4e2c6824016d901788a7cef4d5493dd402
35569 <35569@DESKTOP-3RRV2NK>
2020-08-13 18:34:08 +0200

    Created CRUD.Closes #7

--- CameraManagementSystem/src/main/java/com/esh/CameraManagementSystem/controller/CameraController.java
+++ CameraManagementSystem/src/main/java/com/esh/CameraManagementSystem/controller/CameraController.java
@@ -1,5 +1,65 @@
 package com.esh.CameraManagementSystem.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.esh.CameraManagementSystem.model.Camera;
+import com.esh.CameraManagementSystem.service.CameraServices;
+
+@RestController
 public class CameraController {
+
+	@Autowired
+	CameraServices cameraService;
+
+
+	@PostMapping(value="/addCamera")
+	public String addCamera(@RequestBody Camera camera) {
+		cameraService.createCamera(camera);
+		return "Camera with name "+camera.getName()+" created succesfuly";
+	}
+	@GetMapping(value="/allCameras")
+	public List<Camera> findAllCameras(){
+		return cameraService.getAllCameras();
+	}
+	@GetMapping(value="/cameraById/{id}")
+	public Camera findCameraById(@PathVariable Long id) {
+		Camera camera = null;
+		if(id != null) {
+			camera = cameraService.getCameraById(id);
+		}
+		return camera;
+	}
+	@DeleteMapping(value="/deleteCameraById/{id}")
+	public String deleteCameraById(@PathVariable Long id) {
+		String result = "";
+		if(id != null) {
+			Camera camera = cameraService.getCameraById(id);
+			if(camera != null) {
+				cameraService.deleteCameraById(id);
+				result = "Succesfuly deleted";
+			}else {
+				result = "Not exist camera with id " +id;
+			}
+		}else {
+			result = "Specify id";
+		}
+		return result;
+
+	}
+	@PutMapping(value = "updateCamera/{id}")
+	public String updateCamera(@Validated @RequestBody Camera camera) {
+		cameraService.updateCamera(camera);
+		return "Sucessfuly update";
+	}
 
 }
--- CameraManagementSystem/src/main/java/com/esh/CameraManagementSystem/service/CameraServices.java
+++ CameraManagementSystem/src/main/java/com/esh/CameraManagementSystem/service/CameraServices.java
@@ -1,8 +1,47 @@
 package com.esh.CameraManagementSystem.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.esh.CameraManagementSystem.model.Camera;
+import com.esh.CameraManagementSystem.repository.CameraRepository;
 
 @Service
 public class CameraServices {
+	
+	@Autowired
+	CameraRepository cameraRepository;
+	//Create camera
+	public void createCamera(Camera camera) {
+		cameraRepository.insert(camera);
+	}
+	
+	//Get All Cameras
+	public List<Camera> getAllCameras(){
+		List<Camera> result = cameraRepository.findAll();
+		return result;
+		
+	}
+	//Get Camera By Id
+	public Camera getCameraById(Long id) {
+		Camera camera = null;
+		try {camera = cameraRepository.findById(id).get();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+			return camera ;
+		
+	}
+	//Delete Camera by id
+	public void deleteCameraById(Long id) {
+		cameraRepository.deleteById(id);
+	}
+	//Update Camera by id
+	public void updateCamera(Camera camera) {
+		cameraRepository.save(camera);
+	}
+	
 
 }
