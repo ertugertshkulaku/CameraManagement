0399b41a0076612fc9d851af3654de91ae1b3cb3
35569 <35569@DESKTOP-3RRV2NK>
2020-08-13 15:28:08 +0200

    Created Camera Repository. Closed #4

---
+++ CameraManagementSystem/src/main/java/com/esh/CameraManagementSystem/repository/CameraRepository.java
@@ -0,0 +1,10 @@
+package com.esh.CameraManagementSystem.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.esh.CameraManagementSystem.model.Camera;
+@Repository
+public interface CameraRepository extends MongoRepository<Camera, Long> {
+
+}
